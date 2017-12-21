6503e0f4e5441bb4e0be5c4bee52583f02762f6e
NeilTanado <neiltanado@yahoo.co.id>
2017-12-21 12:57:15 +0700

    edit exe-4

--- Exercise-4.js
+++ Exercise-4.js
@@ -1,7 +1,18 @@
+console.log('LOOPING PERTAMA')
+
 var jumlah = 2;
 var deret = 2;
-
-console.log('LOOPING PERTAMA')
-while(jumlah < 22) { 
+while(jumlah < 22) {
   console.log(jumlah + ' - I Love Coding');
   jumlah+=deret};
+
+
+  console.log('LOOPING KEDUA')
+
+  var jumlah = 22;
+  var deret = 2;
+
+  while(jumlah > 2) {
+    jumlah-=deret;
+    console.log(jumlah + ' - I will become fullstack developer');
+    }
